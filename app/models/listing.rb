ddb6370fab033bc1ef75c8cb94547c1ebb0df8cb
Kyle <KyleZiolkowski90@gmail.com>
2017-01-17 19:47:00 -0500

    etsydemo

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ApplicationRecord
 	has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
 
