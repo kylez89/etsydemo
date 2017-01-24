bdbf959d22a1a63e7d652a9735030739f67ab58a
Kyle <KyleZiolkowski90@gmail.com>
2017-01-24 18:24:16 -0500

    going back to validation errors

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,7 +9,7 @@ class Listing < ApplicationRecord
    	end
 
    	validates :name, :description, :price, presence: true
-   	validates :price, numericality: { greater_than: 0}
+   	validates :price, numericality: { greater_than: 0 }
    	validates_attachment_presence :image
 end
 
